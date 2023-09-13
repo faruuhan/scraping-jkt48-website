b2281d057b77460ad58946fa5b099c1291921a57
faruuhan <farhnn.r@gmail.com>
2023-09-13 13:57:30 +0700

    add endpoin get news

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@ import express from "express";
 const router = express();
 import schedule from "../controllers/schedule.js";
 import member from "../controllers/member.js";
+import news from "../controllers/news.js";
 
 router.get("/schedule", schedule.getSchedule);
 router.get("/schedule/detail/:idschedule", schedule.getDetailSchedule);
 router.get("/member", member.getAllMember);
 router.get("/member/detail/:idmember", member.getDetailMember);
+router.get("/news", news.getAllNews);
 
 export default router;
