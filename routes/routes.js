22a5a01fe4ce9d23420ce6a7afeb55ad6e7e3206
faruuhan <farhnn.r@gmail.com>
2023-09-14 11:40:54 +0700

    Add endpoin get schedule theater

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,7 @@ import news from "../controllers/news.js";
 
 router.get("/schedule", schedule.getSchedule);
 router.get("/schedule/detail/:idschedule", schedule.getDetailSchedule);
+router.get("/schedule/theater", schedule.getTheaterSchedule);
 router.get("/member", member.getAllMember);
 router.get("/member/detail/:idmember", member.getDetailMember);
 router.get("/news", news.getAllNews);
